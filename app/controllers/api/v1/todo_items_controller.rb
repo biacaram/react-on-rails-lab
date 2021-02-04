e9c4bac3ed3c3165a6aa32a3120be821cf75e573
beatriz andre <biacaram@hotmail.com>
2021-02-04 16:57:37 -0300

    index and authentications

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
   def index
+    @todo_items = current_user.todo_items.all
   end
+
   def show
   end
   def create
@@ -10,8 +13,25 @@ class Api::V1::TodoItemsController < ApplicationController
   end
   def destroy
   end
+
   private
+
     def set_todo_item
       @todo_item = TodoItem.find(params[:id])
+    end
+
+    def authorized?
+      @todo_item.user == current_user
+    end
+
+    # This method checks to see if the request is authorized by calling our authorized? private method. 
+    # If the request is not authorized, we return our unauthorized.json.jbuilder view. 
+    # We also pass a status of 401. It's our responsibly to return the correct HTTP status code when building our API.
+    def handle_unauthorized
+      unless authorized?
+        respond_to do |format|
+          format.json { render :unauthorized, status: 401 }
+        end
+      end
     end
 end
---
+++ app/views/api/v1/todo_items/unauthorized.json.jbuilder
@@ -0,0 +1,2 @@
+# This will return a JSON object with an error key with a value of "You are not authorized to perform this action."
+json.error "You are not authorized to perform this action."
