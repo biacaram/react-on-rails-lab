29a4fb6e4dd2a0d8c365dc7886fc738651ed2863
beatriz andre <biacaram@hotmail.com>
2021-02-09 15:40:46 -0300

    getTodoItems

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from "axios";
 
 class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoItems: []
+    };
+    this.getTodoItems = this.getTodoItems.bind(this);
+  }
+  componentDidMount() {
+    this.getTodoItems();
+  }
+  getTodoItems() {
+    axios
+      .get("/api/v1/todo_items")
+      .then(response => {
+        const todoItems = response.data;
+        this.setState({ todoItems });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
     return <p>TodoApp</p>
   }
